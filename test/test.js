c3a33d69bdc113b077aede651c1e4727e84f6beb
Oussama <epiccoder55@gmail.com>
2024-02-01 10:32:16 +0100

    working with server

--- test/test.js
+++ test/test.js
@@ -1,5 +1,8 @@
+var rs = require('readline-sync');
 var aNewNum = 30;
 var aNewNewNum = 20;
-console.log(aNewNum + aNewNewNum)
+console.log(aNewNum + aNewNewNum);
 
+var name = rs.question('what is your name?: ' )
+console.log(`Hello ${ name}, nice to meet you`)
 
